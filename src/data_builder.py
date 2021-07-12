0badc62fc67a6acbf266304298cd2aea95da0ca4
Nathan Kuneman <Nathan.Kuneman@yahoo.com>
2021-07-12 17:27:49 -0600

    cleaning almost completed

--- src/data_builder.py
+++ src/data_builder.py
@@ -1,16 +1,24 @@
 from os import close
 from pybaseball.lahman import *
+from pybaseball.lahman import pitching as pitch
 from pybaseball import playerid_reverse_lookup
 import pandas as pd
 import numpy as np
 import os
-
+import warnings
 class Constructor:
 
     def __init__(self, file_directory):
         self.file_directory = file_directory
-        self.career_totals = None
-        self.batter_df = None
+        self.career_totals = self.career_numbers()
+        self.pitcher_df = self.pitching_career_totals()
+
+        columns = list(self.career_totals.columns)
+        columns.extend(['fantasy_ppg', 'starting_pitcher_hand', 'ppg_vs_hand', 'fantasy_points'])
+
+        
+        self.batter_df = pd.DataFrame(columns=columns)
+        
     
 
     def career_numbers(self):
@@ -51,35 +59,56 @@ class Constructor:
         career_totals = career_totals.iloc[:,19:]
         career_totals = career_totals.reset_index()
 
-        self.career_totals = career_totals
-
+        return career_totals
+       
+        
+    
     def create_training_dataframe(self):
 
         # Create a blank Dataframe that has all available columns 
         columns = list(self.career_totals.columns)
         columns.append('fantasy_points')
-        self.batter_df = pd.DataFrame(columns=columns)
-        lst_teams = os.listdir('../data/game_data/')
-        for file_path in lst_teams:
-            complete_path = '../data/game_data/' + file_path
+        files = os.listdir('../data/processed_events')
+        num_files = len(files)
+        count = 1
+        for file_path in files[:4]:
+            complete_path = '../data/processed_events/' + file_path
             self.add_game_data(complete_path)
+            print(f'Completed {count} files of {num_files}')
+            count += 1
+        self.batter_df = pd.get_dummies(self.batter_df, columns=['position'], drop_first=True)
 
 
-    def add_game_data(self, file_path):
-        event_file = pd.read_csv(file_path, header=None)
+    def add_game_data(self, complete_path):
+        event_file = pd.read_csv(complete_path, header=None)
         columns = {0:'game_id', 1:'batter', 2:'batter_hand', 3:'pitcher', 4:'pitcher_hand',
-           5:'run_first', 6:'run_second', 7:'run_third', 8:'result', 9:'rbi',
-           10:'first_dest', 11:'second_dest', 12:'third_dest',13:'sb_first',
-           14:'sb_second', 15:'sb_third'}
+        5:'run_first', 6:'run_second', 7:'run_third', 8:'batter_pos', 9:'result', 10:'rbi',
+        11:'first_dest', 12:'second_dest', 13:'third_dest', 14:'sb_first',
+        15:'sb_second', 16:'sb_third'}
 
         event_file = event_file.rename(columns=columns)
         games = event_file['game_id'].unique()
         for game in games:
             current_game = event_file[event_file['game_id'] == game]
+            year = complete_path[-11:-7]
+            eve_files = os.listdir(f'../data/game_data/{year}eve')
+            for file in eve_files:
+                if f'{complete_path[-11: -3]}EV' in file:
+                    text_data = file
             
-            #date, wind_factor, temp, ballpark_rating = stadium_info(event_file_path, game) 
+            #, ballpark_rating needs to be added again
+            date, wind_factor, temp = self.stadium_info(f'../data/game_data/{year}eve/{text_data}', game) 
             
             for batter in current_game['batter'].unique():
+
+                # Convert name to that used by Baseball-Reference
+                bbref_name = playerid_reverse_lookup([batter], key_type='retro')
+                bbref_name = bbref_name['key_bbref']
+                n_index = str(bbref_name).index('\n')
+                bbref_name = str(bbref_name)[5:n_index]
+
+                
+
                 current_batter = current_game[current_game['batter'] == batter]
                 singles = current_batter[current_batter['result'] == 20].shape[0]
                 doubles = current_batter[current_batter['result'] == 21].shape[0]
@@ -90,17 +119,64 @@ class Constructor:
                 walks = current_batter[current_batter['result'] == 14].shape[0]
                 sb = self.calc_sb(current_game, batter)
                 hbp = current_batter[current_batter['result'] == 16].shape[0]
+                
+                starting_pitch_rpip = self.get_pitcher_era(current_batter.iloc[0]['pitcher'])
+                
+
+                games = self.batter_df[self.batter_df['playerID'] == bbref_name].shape[0]
+                total_fantasy_points = self.batter_df[self.batter_df['playerID'] == bbref_name].sum()['fantasy_points']
+                if games > 0:
+                    points_per_game = total_fantasy_points/games
+                else:
+                    points_per_game = 0
+
+                starting_pitcher_hand = current_batter.iloc[0, 4]
+                total_vs_pitch_hand = self.batter_df[(self.batter_df['playerID'] == bbref_name) & (self.batter_df['starting_pitcher_hand'] == starting_pitcher_hand)].sum()['fantasy_points']
+                games_vs_pitch_hand = self.batter_df[(self.batter_df['playerID'] == bbref_name) & (self.batter_df['starting_pitcher_hand'] == starting_pitcher_hand)].shape[0]
+
+        
+                if games_vs_pitch_hand > 0:
+                    ppg_vs_hand = total_vs_pitch_hand / games_vs_pitch_hand
+                else:
+                    ppg_vs_hand = points_per_game
+
+                
+
                 fantasy_points = 3*singles + 6*doubles + 9*triples + 12*hrs + 3.5*rbis + 3.2*runs + 3*walks + 6*sb + 3*hbp
-                
-                # Convert name to that used by Baseball-Reference
-                bbref_name = playerid_reverse_lookup([batter], key_type='retro')
-                bbref_name = bbref_name['key_bbref']
-                n_index = str(bbref_name).index('\n')
-                
+
+
                 # Add career numbers to the fantasy data
-                career_nums = self.career_totals[self.career_totals['playerID'] == str(bbref_name)[5:n_index]]
+                career_nums = self.career_totals[self.career_totals['playerID'] == str(bbref_name)]
+                career_nums['fantasy_ppg'] = float(points_per_game)
+                career_nums['ppg_vs_hand'] = float(ppg_vs_hand)
+                career_nums['starting_pitcher_hand'] = starting_pitcher_hand
                 career_nums['fantasy_points'] = fantasy_points
+                career_nums['wind_factor'] = wind_factor
+                career_nums['temp'] = temp
+                career_nums['starting_p_rpip'] = starting_pitch_rpip
+
+                career_nums['position'] = current_batter.iloc[0]['batter_pos']
                 self.batter_df = self.batter_df.append(career_nums)
+
+
+    def pitching_career_totals(self):
+        pitching = pitch()
+        pitching = pitching.groupby('playerID').sum()
+        pitching['runs_p_ip'] = pitching['R'] / (pitching['IPouts'] / 3)
+        pitching = pitching.reset_index()
+        return pitching
+
+
+    def get_pitcher_era(self, pitcher_name):
+        bbref_name = playerid_reverse_lookup([pitcher_name], key_type='retro')
+        bbref_name = bbref_name['key_bbref']
+        n_index = str(bbref_name).index('\n')
+        bbref_name = str(bbref_name)[5:n_index]
+
+        pitcher_stats = self.pitcher_df[self.pitcher_df['playerID'] == bbref_name]
+        
+        return pitcher_stats['runs_p_ip'].mean()
+        
 
 
     def calc_runs(self, game_df, batter, hrs):
@@ -121,7 +197,7 @@ class Constructor:
         return sb
 
 
-    def stadium_info(event_file_path, game_id):
+    def stadium_info(self, event_file_path, game_id):
         # Helper function that returns information about the stadium being played in 
         f = open(event_file_path)
         text = f.read()
@@ -146,7 +222,7 @@ class Constructor:
         
         temp_idx = text.index('info,temp,', game_idx)
         temp = int(text[temp_idx+10:temp_idx+12])
-        close(event_file_path)
+        f.close()
     
         return date, wind_factor, temp, #ballpark_factor
 
@@ -154,8 +230,9 @@ class Constructor:
         self.batter_df.to_csv(file_path)
 
 if __name__ == '__main__':
-    constructor = Constructor('../data/game_data/')
-    constructor.career_numbers()
+    warnings.filterwarnings('ignore')
+    constructor = Constructor('../data/processed_events')
     constructor.create_training_dataframe()
-    print(constructor.batter_df)
+    constructor.save_dataframe('../data/completed_df.csv')
+
     
--- src/event_convertor/convert_event_files.py
+++ src/event_convertor/convert_event_files.py
@@ -1,24 +1,24 @@
 import os
 import subprocess
 import shutil
-import tempfile
+
 import sys
 
-directory = '../data/game_data'
+directory = '../../data/game_data'
 
 
 subfolders = [ f.path for f in os.scandir(directory) if f.is_dir() ]
 
 for folder in subfolders:
-    bevent = r'bevent/BEVENT.EXE'
+    bevent = r'../bevent/BEVENT.EXE'
     shutil.copyfile(bevent, f'{folder}/BEVENT.EXE')
 
     year = folder[-7:-3]
 
 
     # Create a folder to store the processed event logs
-    if not os.path.isdir('../data/processed_events'):
-        os.makedirs('../data/processed_events')
+    if not os.path.isdir('../../data/processed_events'):
+        os.makedirs('../../data/processed_events')
     
     event_files = os.listdir(folder)
     for file in event_files.copy():
@@ -30,8 +30,10 @@ for folder in subfolders:
 
     for file in event_files:
         team = file
-        process = subprocess.Popen([f'wine {folder}/BEVENT.EXE -y {year} -f 0,10,11,14,15,26,27,28,34,43,59,60,61,66,67,68 {folder}/{file} > ../data/processed_events/{file[:-4]}.txt'], shell=True)
-        #process.terminate()
+        process = subprocess.Popen([f'wine {folder}/BEVENT.EXE -y {year} -f 0,10,11,14,15,26,27,28,32,34,43,59,60,61,66,67,68 {folder}/{file} > ../../data/processed_events/{file[:-4]}.txt'], shell=True)
+        
+        
+process.terminate()
     
 
 
